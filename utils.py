d65ffa46d825a28c882174e9e13681174e31575c
devhoodit <72541438+devhoodit@users.noreply.github.com>
2022-09-30 00:38:16 +0900

    feature utils/device-env

---
+++ utils.py
@@ -0,0 +1,26 @@
+import platform
+from torch import cuda
+
+def device_env():
+
+    print(f"Platform : {platform.system()}")
+
+    try:
+        from cpuinfo import get_cpu_info
+
+        info = get_cpu_info()
+
+        print(f"Processor : {info['brand_raw']} - {info['arch_string_raw']}")
+    except ImportError:
+        print("Can't import cpuinfo module, please install py-cpuinfo - 'pip install py-cpuinfo'")
+
+    if cuda.is_available():
+        print("CUDA is available")
+        for i in range(cuda.device_count()):
+            print(f"{cuda.get_device_name(i)}")
+    else:
+        print("CUDA is unavailable")
+
+
+if __name__ == "__main__":
+    device_env()
