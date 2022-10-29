8dfbc562bd0c6c0f9883561049f85eac2d4b36d0
devhoodit <hooditgames@gmail.com>
2022-10-29 23:54:41 +0900

    feature testnet

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Install conda
 ```
 conda create -n "ml2" python=3.9
 conda activate "ml2"
-conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
+conda install pytorch==1.12.0 torchvision==0.13.0 torchaudio==0.12.0 cudatoolkit=10.2 -c pytorch
 ```
 
 Set VSC default python interpreter as "ml2" python 3.9.10
---
+++ ml2/testnet-cifar10.py
@@ -0,0 +1,47 @@
+import argparse
+
+import torch.optim as optim
+import torch.nn as nn
+
+from torchmetrics.functional.classification import accuracy
+from src.engines import train_summary1
+
+from src import utils
+from src import datasets
+from src.models import TestNet
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="resnet18 with cifar-10")
+    utils.arg_setting(parser)
+    parser.add_argument("--title", default="summary1", type=str)
+    args = parser.parse_args()
+    
+    utils.device_env()
+    
+    # base setting
+    device = utils.device_setting(args.device)
+    print(f"Torch running on {device}")
+    
+    # Load dataset with dataloader
+    cifar_dataload = datasets.CIFAR10.load_train_cifar_10(root=args.data, batch_size=4, shuffle=True, num_workers=args.numworkers)
+
+    
+    # Build Model
+    model = TestNet()
+    model = model.to(device)
+    
+    
+    # Build optimizer
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs * len(cifar_dataload))
+    loss_fn = nn.CrossEntropyLoss()
+    metric_fn = accuracy
+    
+    # Loop
+    for epoch in range(args.epochs):
+        summary = train_summary1(cifar_dataload, model, optimizer, scheduler, loss_fn, metric_fn, device)
+        print(f'Epoch: {epoch + 1}, Accuracy: {summary["metric"]:.4f}')
+        utils.save_checkpoint(args.checkpoints, args.title, model, optimizer, epoch + 1)
+    
+    
+    
