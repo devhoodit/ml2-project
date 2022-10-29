d94173a845cfeb7f5c171dea5838c904516f7ec1
devhoodit <hooditgames@gmail.com>
2022-10-30 04:58:49 +0900

    fix

--- ml2/src/global_settings.py
+++ ml2/src/global_settings.py
@@ -2,10 +2,9 @@ from torchvision import transforms as T
 
 data_augmentation = T.Compose(
     [
-        T.ToPILImage(),
+        T.ToTensor(),
         T.RandomHorizontalFlip(p=0.3),
         T.RandomVerticalFlip(p=0.3),
-        T.ToTensor(),
         T.GaussianBlur(3),
         T.RandomErasing(p=0.3),
         T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
