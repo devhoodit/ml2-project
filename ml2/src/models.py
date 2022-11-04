9ac9b4a8c3d66ba03afad220e8b9b1f813e23ded
devhoodit <72541438+devhoodit@users.noreply.github.com>
2022-11-04 16:49:42 +0900

    fix models/dataaugnetnn-forward

--- ml2/src/models.py
+++ ml2/src/models.py
@@ -123,6 +123,15 @@ class DataAugNetNN(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 60)
         self.fc3 = nn.Linear(60, 2)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
