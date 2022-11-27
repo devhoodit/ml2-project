bd8a3cf624045f32e0858da9b425c9c66d76e347
devhoodit <hooditgames@gmail.com>
2022-11-27 20:32:01 +0900

    feature ml3/mnet

--- ml3/src/model.py
+++ ml3/src/model.py
@@ -70,3 +70,45 @@ class MNIST:
             output = F.log_softmax(x, dim=1)
             
             return output
+        
+    class MNet(nn.Module):
+        def __init__(self) -> None:
+            super().__init__()
+            self.conv1 = nn.Conv2d(1, 5, 3, padding='same')
+            self.conv2 = nn.Conv2d(5, 9, 3, padding='same')
+            self.conv3 = nn.Conv2d(9, 13, 3, padding='same')
+            self.pool = nn.MaxPool2d(2, 2)
+            self.fc1 = nn.Linear(7*7*13, 500)
+            self.fc2 = nn.Linear(500, 400)
+            self.fc3 = nn.Linear(400, 300)
+            self.fc4 = nn.Linear(300, 150)
+            self.fc5 = nn.Linear(150, 10)
+            
+        def forward(self, x):
+            x = self.conv1(x)
+            x = F.relu(x)
+            x = self.pool(x)
+            x = self.conv2(x)
+            x = F.relu(x)
+            x = self.pool(x)
+            x = self.conv3(x)
+            x = F.relu(x)
+            
+            x = torch.flatten(x, 1)
+            
+            x = self.fc1(x)
+            x = F.relu(x)
+            
+            x = self.fc2(x)
+            x = F.relu(x)
+            
+            x = self.fc3(x)
+            x = F.relu(x)
+            
+            x = self.fc4(x)
+            x = F.relu(x)
+            
+            x = self.fc5(x)
+            output = F.log_softmax(x, dim=1)
+            
+            return output
